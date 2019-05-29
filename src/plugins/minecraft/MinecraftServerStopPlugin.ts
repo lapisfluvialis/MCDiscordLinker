757fc22b2a6a78dcdb4450266520c898f6e88ad1
lapisfluvius <39161166+lapisfluvius@users.noreply.github.com>
2019-05-30 02:08:32 +0900

    1.14 support

--- src/plugins/minecraft/MinecraftServerStopPlugin.ts
+++ src/plugins/minecraft/MinecraftServerStopPlugin.ts
@@ -2,7 +2,7 @@ import MinecraftPlugin from "../../MinecraftPlugin";
 import MCDLLocalizableString from "../../MCDLLocalizableString";
 
 export default class MinecraftServerStopPlugin extends MinecraftPlugin {
-    public readonly matcher: RegExp = /\[..:..:..\] \[Server Shutdown Thread\/INFO\]: Stopping server/;
+    public readonly matcher: RegExp = /\[..:..:..\] \[.+?\/INFO\]: Stopping server/;
     protected matched(match: string[]): void {
         this.discordClient.sendf(this.localizable, match);
     }
